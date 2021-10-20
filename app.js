5dbcb592272c639f166b527bd2e25e166500a4c0
comolokko <samederturk55@gmail.com>
2021-10-20 19:04:12 +0300

    Ders sonu

---
+++ app.js
@@ -0,0 +1,36 @@
+console.log("Hello World")
+
+
+let dolarBugun = 9.10
+let dolarDun = 9.20
+
+{
+    let dolarDun = 9.30
+}
+
+console.log(dolarDun)
+
+const euroDun = 11.2
+//EuroDun = 10  //const olduğu için başka değer veremeyiz. HATAA
+
+console.log(euroDun)
+
+//array = dizi
+//camelCasing
+//PascalCasing
+
+let konutKredileri = ["Konut Kredisi", "emlak konut","kamu konut" ]
+
+
+console.log("<ul>")
+for (let i= 0; i < konutKredileri.length ; i++) 
+{
+    console.log("<li>" + konutKredileri[i] + "</li>")   
+}
+console.log("</ul>")
+console.log(konutKredileri)
+
+
+
+
+
